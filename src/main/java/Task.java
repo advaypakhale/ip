ecda55654c192eda57ab77946e89caee7c7fb09f
advaypakhale <advay.pakhale@gmail.com>
2025-01-27 16:07:45 +0800

    feat: Add Deadline, Event, Todo classes

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,7 +7,7 @@ public class Task {
         this.isComplete = false;
     }
 
-    public String getStatusIcon() {
+    protected String getStatusIcon() {
         return (isComplete ? "X" : " "); // mark done task with X
     }
 
@@ -22,5 +22,43 @@ public class Task {
 
     public void markAsUndone() {
         this.isComplete = false;
+    }
+}
+
+class Todo extends Task {
+    public Todo(String description) {
+        super(description);
+    }
+
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    private String due;
+
+    public Deadline(String description, String due) {
+        super(description);
+        this.due = due;
+    }
+
+    public String toString() {
+        return "[D]" + super.toString() + " (Deadline: " + due + " )";
+    }
+}
+
+class Event extends Task {
+    private String begin;
+    private String end;
+
+    public Event(String description, String begin, String end) {
+        super(description);
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public String toString() {
+        return "[E]" + super.toString() + " (Event start: " + begin + " | Event end: " + end + ")";
     }
 }
