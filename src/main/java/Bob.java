fca00f052a43936990dcf46042f0c1cd57e9cc39
advaypakhale <advay.pakhale@gmail.com>
2025-01-27 14:20:38 +0800

    refactor: Change duke to bob

---
+++ src/main/java/Bob.java
@@ -0,0 +1,12 @@
+public class Bob {
+    public static void main(String[] args) {
+        String logo = """
+                    ____        __
+                   / __ )____  / /_\s
+                  / __  / __ \\/ __ \\
+                 / /_/ / /_/ / /_/ /
+                /_____/\\____/_.___/\s
+                """;
+        System.out.println("Hello from\n" + logo);
+    }
+}
