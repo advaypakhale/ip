376f611c111906beaed1fced6d989774cba4ee99
advaypakhale <advay.pakhale@gmail.com>
2025-01-27 14:48:57 +0800

    feat: Add main loop logic for greet and exit

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,5 +1,30 @@
 public class Bob {
+
+    private Boolean isActive = false;
+
+    public Bob() {
+        this.isActive = true;
+    }
+
     public static void main(String[] args) {
+        Bob bob = new Bob();
+        bob.run();
+    }
+
+    public void run() {
+        greet();
+        exit();
+    }
+
+    public void encapsulateSection(String message) {
+        System.out.println("-----------------------------");
+        System.out.println(message);
+        System.out.println("-----------------------------");
+    }
+
+    public void greet() {
+        String message = "";
+
         String logo = """
                     ____        __
                    / __ )____  / /_\s
@@ -7,6 +32,16 @@ public class Bob {
                  / /_/ / /_/ / /_/ /
                 /_____/\\____/_.___/\s
                 """;
-        System.out.println("Hello from\n" + logo);
+        message += logo;
+
+        message += "Hello! I'm Bob, but you can call me Bob.\n";
+        message += "What can I do for you on this fine day?";
+
+        encapsulateSection(message);
+    }
+
+    public void exit() {
+        String message = "Goodbye! Have a great day!";
+        encapsulateSection(message);
     }
 }
